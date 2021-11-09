4c6496fff120e7e42f8602ac59accf192090b485
Baubek <baukenzo16@gmail.com>
2021-11-10 02:18:23 +0600

    endAllSection

--- app/js/main.js
+++ app/js/main.js
@@ -30,12 +30,64 @@ $('.slider-map').slick({
     focusOnSelect: true,
 })
 
-$('.travel__slider').slick({
+$('.holder__slider, .shop__slider').slick({
     infinite: true,
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
 })
 
-  
+
+$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/Plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/Minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    $('.quantity-button').on('click', function () {
+		var parents = $(this).parents('.holder-slider__info');
+		let summ = $('.nights', parents).val() * $('.summ', parents).data('guests') * $('.guests', parents).val();
+		$('.summ', parents).html('$' + summ);
+	});
+
+	$('.quantity').each(function () {
+		var parents = $(this).parents('.holder-slider__info');
+		let summ = $('.nights', parents).val() * $('.summ', parents).data('guests') * $('.guests', parents).val();
+		$('.summ', parents).html('$' + summ);
+	});
+
+
+    
+    });
+
+    $('.surfboard-box__circle').on('click', function(){
+        $(this).toggleClass('active')
+    });
+
 });
+
